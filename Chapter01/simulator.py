97f6dda5dc3b83642cf01329d504b7cb2964785a
CodeMask <mattode@outlook.com>
2020-11-03 06:43:09 +0100

    unit test

--- Chapter01/simulator.py
+++ Chapter01/simulator.py
@@ -1,5 +1,9 @@
+from matplotlib import use
+use('TKAgg')
+
 from matplotlib import pyplot as plt
 from matplotlib import animation
+
 
 
 class Particle:
@@ -39,8 +43,8 @@ class ParticleSimulator:
 
 def visualize(simulator):
 
-    x = [p.x for p in simulator.particles]
-    y = [p.y for p in simulator.particles]
+    X = [p.x for p in simulator.particles]
+    Y = [p.y for p in simulator.particles]
 
     fig = plt.figure()
     ax = plt.subplot(111, aspect='equal')
@@ -82,6 +86,30 @@ def test_visualize():
 
     simulator = ParticleSimulator(particles)
     visualize(simulator)
+
+
+def test_evolve():
+    particles = [Paricle(0.3, 0.5, +1),
+                Particle(0.0, -0.5, -1),
+                Particle(-0.1, -0.4, +3)]
+
+    simulator = ParticleSimulator(particles)
+
+    simulator.evolve(0.1)
+
+    p0, p1, p2 = particles
+
+    def fequal(a, b, eps=1e-5):
+        return abs(a - b) < eps
+
+    assert fequal(p0.x, 0.210269)
+    assert fequal(p0.y, 0.543863)
+
+    assert fequal(p1.x, -0.099334)
+    assert fequal(p1.y, -0.490034)
+
+    assert fequal(p2.x, 0.191358)
+    assert fequal(p2.y, -0.365227) 
 
 
 if __name__ == "__main__":
