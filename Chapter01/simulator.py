37f3c85e1a6d3a396479da804662749be0915815
CodeMask <mattode@outlook.com>
2020-11-03 06:18:10 +0100

    simulator visualizer

--- Chapter01/simulator.py
+++ Chapter01/simulator.py
@@ -1,3 +1,6 @@
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
 
 class Particle:
 
@@ -33,4 +36,41 @@ class ParticleSimulator:
                 p.y += d_y
                 # 3. repeat for all the time steps
 
-                
+
+def visualize(simulator):
+
+    x = [p.x for p in simulator.particles]
+    y = [p.y for p in simulator.particles]
+
+    fig = plt.figure()
+    ax = plt.subplot(111, aspect='equal')
+    line, = ax.plot(X, Y, 'ro')
+
+    # Axis limits
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
+
+    # It will run when the animation starts
+    def init():
+        line.set_data([], [])
+        return line, # the comma is important!
+
+    
+    def animate(i):
+        # We let the particle evolve for 0.01 time units
+        simulator.evolve(0.01)
+        X = [p.x for p in simulator.particles]
+        Y = [p.y for p in simulator.particles]
+
+        line.set_data(X, Y)
+        return line, 
+
+    # call the animate function each 10 ms
+    anim = animation.FuncAnimation(fig,
+                                    animate,
+                                    init_func=init,
+                                    blit=True,
+                                    interval=10)
+
+    plt.show()
+
